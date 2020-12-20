c93b454b7b2fb4fdc7e9c95d7185bf8f4f3abfd6
James Monks <J.monks23@gmail.com>
2020-12-20 11:34:00 +0000

    added vanilla js file, set up variables for the inputs/buttons

---
+++ ionic-budget-planner/app.js
@@ -0,0 +1,8 @@
+const reasonInput = document.querySelector('#input-expense');
+const amountInput = document.querySelector('#input-amount');
+const clearBtn = document.querySelector('#btn-clear');
+const addBtn = document.querySelector('#btn-add');
+
+addBtn.addEventListener('click', () => {
+  console.log("It works!");
+});
